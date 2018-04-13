769242d34a0d4c94256e3e9de87e652616cbc531
renb <91renb@gmail.com>
2018-04-13 22:52:18 +0800

    更新代码

--- BRChangeLocationDemo/BRChangeLocationDemo/ViewController.m
+++ BRChangeLocationDemo/BRChangeLocationDemo/ViewController.m
@@ -20,9 +20,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    CLLocationCoordinate2D location2D = CLLocationCoordinate2DMake(30.189829, 120.182165);
+    // 如果要想知道任意位置的坐标，去高德地图 http://lbs.amap.com/console/show/picker，选中自己要定位的坐标
+    // 通过高德地图得到的是高德的坐标系，然后再通过代码转换成WGS坐标系，最后去MyLocation.gpx文件里修改经纬度即可
+    // 打卡坐标（高德坐标系值）：（创业软件：120.186108, 30.188364）（宝龙城：120.167714, 30.188638）
+    // 打卡坐标（WGS坐标系值）：120.181512, 30.190756
+//    CLLocationCoordinate2D location2D = CLLocationCoordinate2DMake(30.188638, 120.167714);
 //    CLLocationCoordinate2D WGSlocation2D = [BRChangeLocation gcj02ToWgs84:location2D];
-//    NSLog(@"纬度：%f,经度：%f",WGSlocation2D.latitude , WGSlocation2D.longitude);
+//    NSLog(@"打卡坐标（WGS坐标系值）: (%f, %f)",WGSlocation2D.latitude , WGSlocation2D.longitude);
     
     [self.manager startUpdatingLocation];
 }
@@ -40,28 +44,28 @@
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
     CLLocation *currentLocation = [locations lastObject];
-    // 1.如果打卡地点是这里，当前的经纬度。
-    // 2.直接copy下面的经纬度，无需转换
-    // 3.去Location1.gpx修改经纬度
     // 当前的经纬度
-    NSLog(@"当前的经纬度 %f, %f", currentLocation.coordinate.latitude, currentLocation.coordinate.longitude);
-    
+    NSLog(@"当前的经纬度（WGS坐标系值）： %f, %f", currentLocation.coordinate.latitude, currentLocation.coordinate.longitude);
+    CLLocationCoordinate2D location2D = CLLocationCoordinate2DMake(currentLocation.coordinate.latitude, currentLocation.coordinate.longitude);
+    CLLocationCoordinate2D WGSlocation2D = [BRChangeLocation wgs84ToGcj02:location2D];
+    NSLog(@"当前的经纬度（高德坐标系值）： %f, %f", currentLocation.coordinate.latitude, currentLocation.coordinate.longitude);
+    currentLocation = [[CLLocation alloc]initWithLatitude:WGSlocation2D.latitude longitude:WGSlocation2D.longitude];
     //地理反编码 可以根据坐标(经纬度)确定位置信息(街道 门牌等)
     CLGeocoder *geoCoder = [[CLGeocoder alloc]init];
     [geoCoder reverseGeocodeLocation:currentLocation completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
-        if (placemarks.count >0) {
+        if (placemarks.count > 0) {
             CLPlacemark *placeMark = placemarks[0];
             NSString *currentCity = placeMark.locality;
             if (!currentCity) {
                 currentCity = @"无法定位当前城市";
             }
-            //看需求定义一个全局变量来接收赋值
-            NSLog(@"当前国家 - %@",placeMark.country);//当前国家
-            NSLog(@"当前城市 - %@",currentCity);//当前城市
-            NSLog(@"当前位置 - %@",placeMark.subLocality);//当前位置
-            NSLog(@"当前街道 - %@",placeMark.thoroughfare);//当前街道
-            NSLog(@"具体地址 - %@",placeMark.name);//具体地址
-            NSLog(@"定位地址：%@,%@,%@,%@", currentCity, placeMark.subLocality, placeMark.thoroughfare, placeMark.name);
+            // 看需求定义一个全局变量来接收赋值
+//            NSLog(@"当前国家 - %@",placeMark.country);//当前国家
+//            NSLog(@"当前城市 - %@",currentCity);//当前城市
+//            NSLog(@"当前位置 - %@",placeMark.subLocality);//当前位置
+//            NSLog(@"当前街道 - %@",placeMark.thoroughfare);//当前街道
+//            NSLog(@"具体地址 - %@",placeMark.name);//具体地址
+            NSLog(@"定位地址：%@,%@,%@,%@", currentCity, placeMark.subLocality, placeMark.thoroughfare, placeMark.name);// 高德坐标对应的地址
         }
     }];
 }
